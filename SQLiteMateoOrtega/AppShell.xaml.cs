02d07dff193369314ef2a73f335967ad9edb262e
laboratorio <laboratorio@UPOAULA10718>
2025-06-24 07:44:14 -0500

    Agregar archivos de proyecto.

---
+++ SQLiteMateoOrtega/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLiteMateoOrtega
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SQLiteMateoOrtega/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SQLiteMateoOrtega;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
