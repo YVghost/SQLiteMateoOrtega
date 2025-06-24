e2e5642f10f1b0326ceb4bad3fde45571a6d7fe0
mateo <dddmateo@gmail.com>
2025-06-24 07:52:10 -0500

    modelo contacto

---
+++ SQLiteMateoOrtega/Models/Contacto.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace SQLiteMateoOrtega.Models
+{
+    class Contacto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [SQLite.NotNull]
+        public string CorreoElectronico { get; set; }
+        [SQLite.Column("Correo")]
+        public int Telefono { get; set; }
+        [MaxLength(15)]
+        public string Direccion { get; set; }
+        public string Empresa { get; set; }
+   
+    }
+}
